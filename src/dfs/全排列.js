9d54520769ce97f417dcd78a0e0b11c8fe00b544
springleo <782655835@qq.com>
2020-06-01 23:12:34 +0800

    全排列

---
+++ src/dfs/全排列.js
@@ -0,0 +1,22 @@
+function permutation(arr) {
+    let result = []
+    let len = arr.length
+
+    run(0)
+
+    function run(index) {
+        if (index === len - 1) { // index在末尾时，结束递归，存入结果
+            result.push([...arr])
+            return
+        }
+
+        for (let i = index; i < len; i++) {
+            swap(arr, i, index) // 固定第index位
+            run(index + 1) // 递归开始，index + 1后全排列
+            swap(arr, i, index) // 数组复原
+        }
+    }
+    return result
+}
+
+console.log(permutation(['a', 'b', 'c']))
