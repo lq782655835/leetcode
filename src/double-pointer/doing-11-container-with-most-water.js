af5141c3d6b270575a799f7cd7749f556bbea40f
springleo <782655835@qq.com>
2020-06-01 22:03:06 +0800

    leetcode11

---
+++ src/double-pointer/doing-11-container-with-most-water.js
@@ -0,0 +1,43 @@
+// 11. 盛最多水的容器
+
+// 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+// 说明：你不能倾斜容器，且 n 的值至少为 2。
+
+//  
+
+
+
+// 图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+//  
+
+// 示例：
+
+// 输入：[1,8,6,2,5,4,8,3,7]
+// 输出：49
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/container-with-most-water
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max = 0
+    // 思路：双指针法，当左边
+    while (left < right) {
+        // 最大值最小值时，一定有个变量比对
+        max = Math.max(max, (height[right] - height[left]) * Math.min(height[right], height[left]))
+        // 关键：右侧更高时，左侧前进（看有没有更高的）
+        if (height[right] > height[left]) {
+            left++
+        } else {
+            right--
+        }
+    }
+
+    return max
+};
