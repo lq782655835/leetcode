59cf26675dcb7a7566d3ee376dbda837f5c98f56
springleo <782655835@qq.com>
2020-06-12 23:21:31 +0800

    feat: 21merge two sorted link

---
+++ src/chain-list/21-merge-two-sorted-lists.js
@@ -0,0 +1,41 @@
+// 21. 合并两个有序链表
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+// 示例：
+
+// 输入：1->2->4, 1->3->4
+// 输出：1->1->2->3->4->4
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    let head = tail = new ListNode() // 新链表的头部和尾部
+    // 每次尾部都指向最小node
+    while(l1 && l2) {
+        // l1 和 l2
+        if (l1.val <= l2.val) {
+            tail.next = l1
+            l1 = l1.next
+        } else {
+            tail.next = l2
+            l2 = l2.next
+        }
+        // 尾部更新
+        tail = tail.next
+    }
+    tail.next = l1 ? l1 : l2
+
+    return head.next
+};
