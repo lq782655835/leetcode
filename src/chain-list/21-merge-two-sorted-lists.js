12d6a26083e0401c63b7d547c48604bad04da95d
liaoqiao.lq <liaoqiao.lq@cainiao.com>
2024-05-05 21:50:42 +0800

    feat(): merge two lists

--- src/chain-list/21-merge-two-sorted-lists.js
+++ src/chain-list/21-merge-two-sorted-lists.js
@@ -39,3 +39,24 @@ var mergeTwoLists = function(l1, l2) {
 
     return head.next
 };
+
+
+// 递归方式
+// var mergeTwoLists = function(l1, l2) {
+//     if (l1 === null) {
+//         return l2;
+//     } else if (l2 === null) {
+//         return l1;
+//     } else if (l1.val < l2.val) {
+//         l1.next = mergeTwoLists(l1.next, l2);
+//         return l1;
+//     } else {
+//         l2.next = mergeTwoLists(l1, l2.next);
+//         return l2;
+//     }
+// };
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/merge-two-sorted-lists/solutions/226408/he-bing-liang-ge-you-xu-lian-biao-by-leetcode-solu/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
